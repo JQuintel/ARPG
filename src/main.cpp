c0d34008bc591ce008072d53cb3c46850e0daae7
Mika Goetze <mika.goetze@mightykingdom.com>
2018-12-22 21:51:32 +1030

    Changed project name.

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@ int main() {
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow * window = glfwCreateWindow(800, 600, "Test", nullptr, nullptr);
+    GLFWwindow * window = glfwCreateWindow(800, 600, "ARPG", nullptr, nullptr);
 
     if(!window)
     {
