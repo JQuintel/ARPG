6a5e6fd5aee2c5a0261566f73881436f57c9dc56
Mika Goetze <mika.goetze@mightykingdom.com>
2018-12-22 22:02:34 +1030

    Made some small changes / improvements to main.

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,16 @@
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 
+unsigned int window_width = 800;
+unsigned int window_height = 600;
+
+void OnWindowSizeChanged(GLFWwindow * window, int width, int height)
+{
+    window_width = static_cast<unsigned int>(width);
+    window_height = static_cast<unsigned int>(height);
+    glViewport(0, 0, window_width, window_height);
+}
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
 
@@ -14,7 +24,8 @@ int main() {
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow * window = glfwCreateWindow(800, 600, "ARPG", nullptr, nullptr);
+    GLFWwindow * window = glfwCreateWindow(window_width, window_height, "ARPG", nullptr, nullptr);
+    glfwSetWindowSizeCallback(window, OnWindowSizeChanged);
 
     if(!window)
     {
@@ -30,8 +41,12 @@ int main() {
         return -1;
     }
 
+    glViewport(0, 0, window_width, window_height);
+    glClearColor(0.2, 0.2, 0.2, 1);
+
     while(!glfwWindowShouldClose(window))
     {
+        glClear(GL_COLOR_BUFFER_BIT);
         glfwPollEvents();
         glfwSwapBuffers(window);
     }
